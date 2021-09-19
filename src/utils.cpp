c240a5ec7846d823481c322755b2871d79eaaff1
Pedro Torrente <pedrotorrente@pop-os.localdomain>
2021-09-19 18:00:21 -0300

    Adding parse function

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,22 +1,51 @@
 #include "utils.hpp"
 
-void utils::printJSON(nlohmann::json json)
-{
-    if (json.is_object()) {
-        for (auto& el : json.items()) {
-            if (el.value().is_structured()) {
-                printJSON(el.value());
+namespace utils{
+    /** 
+     * Concatenate json values with a comma separator
+     * @param json_parse json to be parsed
+     * @return parsed value
+     */
+    std::string concat (nlohmann::json json_parse){
+        std::stringstream parsed;
+        for (auto& el : json_parse){
+            if (el.is_structured()){
+                parsed << concat(el);
             } else {
-                std::cout << el.key() << ":" << el.value() << std::endl;
+                parsed << el << ",";
+            }                
+        }
+        return parsed.str();
+
+    }
+    /**
+     * Parse a json to a string in a fixed size
+     * @param json_parse json to be parsed
+     * @param maxsize sized to be delimitated
+     */
+    std::string parse (nlohmann::json json_parse, int maxsize){
+        std::string response = concat(json_parse);
+        response.pop_back();
+
+        int position = 0;
+        while ((position = response.find('"')) != std::string::npos){
+            response.erase(position, 1);
+        }
+        
+        if (response.size() > maxsize){
+            response.resize(maxsize);
+            int count = maxsize - 1;
+            while (response[count] != ','){
+                response[count] = SYMBOL;
+                count--;
+            }
+            response[count] = SYMBOL;
+        } else {
+            for(int local = response.size() - 1; local < maxsize; local++){
+                response.push_back(SYMBOL);
             }
         }
-    } else {
-        for (auto& el : json) {
-            if (el.is_structured()) {
-                printJSON(el);
-            } else {
-                std::cout << el << std::endl;
-            }
-        }
+
+        return response;
     }
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,7 +1,11 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <nlohmann/json.hpp>
+#define SYMBOL '#'
 
 namespace utils{
-    void printJSON(nlohmann::json json);
+    std::string concat (nlohmann::json json_parse);
+    std::string parse (nlohmann::json json_parse, int maxsize);
 }
