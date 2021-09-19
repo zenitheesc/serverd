fa19006ccecc56f42a747c1b58c70a0334cccf6a
Arthur Grillo <arthur.grillo@usp.br>
2021-09-19 19:10:26 -0300

    Addded arguments to the server and changed the errors messages

--- src/server.cpp
+++ src/server.cpp
@@ -1,52 +1,44 @@
+#include "utils.hpp"
 #include <httplib.h>
 #include <nlohmann/json.hpp>
+#include <string>
 #include <thread>
 #include <exception>
 
-void printJSON(nlohmann::json json){
-    if(json.is_object()){
-        for(auto& el : json.items()){
-            if (el.value().is_structured()) printJSON(el.value());
-            else std::cout << el.key() << ":" << el.value() << std::endl;
-        }
-    }else {
-        for(auto& el : json){
-            if (el.is_structured()) printJSON(el);
-            else std::cout << el << std::endl;
-        }
-    }
-    
-}
+int main(int argc, char* argv[]){
+    const char* host;
+    int port;
 
-void server(){
+    if(argc < 2){
+        host = "localhost";
+        port = 8080;
+    }else if (argc == 3) {
+        host = argv[1];
+        port = atoi(argv[2]);
+    }
+    else{
+        throw std::invalid_argument("invalid number of arguments");
+    }
+
     httplib::Server svr;
 
     svr.Post("/", [](const httplib::Request& req, httplib::Response& res){
         nlohmann::json receivedJson;
         receivedJson = nlohmann::json::parse(req.body);
-        printJSON(receivedJson);
+        utils::printJSON(receivedJson);
         std::string remote_addr = req.remote_addr;
-        res.set_content("mensagem recebida ^-^", "text/plain");
+        res.set_content("received message", "text/plain");
     });
     
     svr.set_error_handler([](const httplib::Request&, httplib::Response &res) {
-        std::string html = R"(<h1>NADA PARA VER AQUI</h1><a><img src="https://zenith.eesc.usp.br/images/Footer/logo.svg" alt="Zenith Logo" width="148px" height="79px" style="background-color: black;"></a>)";
-        res.set_content(html, "text/html");
+        std::string error = "Error:" + std::to_string(res.status);
+        res.set_content(error,"text/plain");
     });
 
     svr.set_exception_handler([](const httplib::Request& req, httplib::Response& res, std::exception& e){
             std::cout << e.what() << std::endl;
-            res.set_content("alguma coisa n funcionou, vai reclamar com o Matheus", "text/plain");
+            res.set_content("exception raised", "text/plain");
     });
 
-    svr.listen("localhost", 1234);
-}
-
-int main(void){
-    std::thread thread(server);
-    while (true) {
-        sleep(5);
-        std::cout << "\toi\n";
-    }
-    thread.join();
+    svr.listen(host, port);
 }
