6a7371c6f75bd86f1009ed66f562800f2981d03a
Arthur Grillo <arthur.grillo@usp.br>
2021-09-24 16:15:14 -0300

    Started using clang-tidy and clang-formatter

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,16 +1,22 @@
 #include "utils.hpp"
 
-void utils::printJSON(nlohmann::json json){
-    if(json.is_object()){
-        for(auto& el : json.items()){
-            if (el.value().is_structured()) printJSON(el.value());
-            else std::cout << el.key() << ":" << el.value() << std::endl;
+void utils::printJSON(nlohmann::json json)
+{
+    if (json.is_object()) {
+        for (auto& el : json.items()) {
+            if (el.value().is_structured()) {
+                printJSON(el.value());
+            } else {
+                std::cout << el.key() << ":" << el.value() << std::endl;
+            }
         }
-    }else {
-        for(auto& el : json){
-            if (el.is_structured()) printJSON(el);
-            else std::cout << el << std::endl;
+    } else {
+        for (auto& el : json) {
+            if (el.is_structured()) {
+                printJSON(el);
+            } else {
+                std::cout << el << std::endl;
+            }
         }
     }
-    
 }
