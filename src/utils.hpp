cac5ce7b93412f0db02cbc7fe347b0528f07e171
Arthur Grillo <arthur.grillo@usp.br>
2021-10-16 21:23:05 -0300

    Made the default dir more redable

--- src/utils.hpp
+++ src/utils.hpp
@@ -12,5 +12,5 @@
 namespace utils {
 auto concat(nlohmann::json json_parse) -> std::string;
 auto parse(nlohmann::json json_parse, int max_size) -> std::string;
-void saveJson(const nlohmann::json& json, std::filesystem::path path = "./");
+void saveJson(const nlohmann::json& json, std::filesystem::path path = std::filesystem::current_path());
 }
