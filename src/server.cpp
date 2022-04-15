6297f8f92abbb44aa6b5645f797e7f24a9fc2863
grillo <Arthur Grillo>
2022-04-15 16:00:08 -0300

    server: refactor the code to use the RoutineService Template from the fkd

--- src/server.cpp
+++ src/server.cpp
@@ -1,10 +1,77 @@
 #include "message-buffer.hpp"
 #include "utils.hpp"
 #include <exception>
+#include <frameworkd/classes/daemon/daemon.hpp>
 #include <httplib.h>
 #include <nlohmann/json.hpp>
 #include <string>
 #include <thread>
+
+class ServerdService : public RoutineService {
+    const char* m_host;
+    int m_port;
+
+    httplib::Server m_svr;
+    MessagesBuffer m_buffer;
+
+    static const int buffer_size = 10;
+
+public:
+    explicit ServerdService(const char* host, const int port)
+        : RoutineService { "serverd" }
+        , m_host { host }
+        , m_port { port }
+        , m_buffer { buffer_size }
+    {
+    }
+
+    const DBusHandler::Path m_requestPath {
+        "zfkd.dbus.serverd",
+        "/zfkd/dbus/serverd",
+        "zfkd.dbus.serverd",
+        "request"
+    };
+
+    void setup() override
+    {
+
+        m_svr.Post("/", [&](const httplib::Request& req, httplib::Response& res) {
+            nlohmann::json receivedJson;
+            receivedJson = nlohmann::json::parse(req.body);
+
+            std::uint8_t id = receivedJson["equipe"];
+            nlohmann::json payload = receivedJson["payload"];
+
+            m_buffer.write(id, payload);
+            utils::saveJson(receivedJson, std::to_string(id));
+
+            res.set_content("received message", "text/plain");
+        });
+
+        m_svr.set_error_handler([](const httplib::Request&, httplib::Response& res) {
+            std::string error = "Error:" + std::to_string(res.status);
+            res.set_content(error, "text/plain");
+        });
+
+        m_svr.set_exception_handler([](const httplib::Request& req, httplib::Response& res, std::exception& e) {
+            std::cout << e.what() << std::endl;
+            res.set_content("exception raised", "text/plain");
+        });
+
+        DBusHandler::registerMethod(m_requestPath, [&](nlohmann::json req) {
+            return m_buffer.getCurrMessage();
+        });
+    }
+
+    void routine() override
+    {
+        m_svr.listen(m_host, m_port);
+    }
+
+    void destroy() override
+    {
+    }
+};
 
 auto main(int argc, char* argv[]) -> int
 {
@@ -21,31 +88,10 @@ auto main(int argc, char* argv[]) -> int
         throw std::invalid_argument("invalid number of arguments");
     }
 
-    httplib::Server svr;
-    MessagesBuffer buffer(10);
+    Daemon serverd { "serverd.json" };
+    ServerdService serverdService { host, port };
+    serverd.deploy(serverdService);
+    serverd.run();
 
-    svr.Post("/", [&buffer](const httplib::Request& req, httplib::Response& res) {
-        nlohmann::json receivedJson;
-        receivedJson = nlohmann::json::parse(req.body);
-
-        int id = receivedJson["equipe"];
-        nlohmann::json payload = receivedJson["payload"];
-
-        buffer.write(id, payload);
-        utils::saveJson(receivedJson, std::to_string(id));
-
-        res.set_content("received message", "text/plain");
-    });
-
-    svr.set_error_handler([](const httplib::Request&, httplib::Response& res) {
-        std::string error = "Error:" + std::to_string(res.status);
-        res.set_content(error, "text/plain");
-    });
-
-    svr.set_exception_handler([](const httplib::Request& req, httplib::Response& res, std::exception& e) {
-        std::cout << e.what() << std::endl;
-        res.set_content("exception raised", "text/plain");
-    });
-
-    svr.listen(host, port);
+    return 0;
 }
